974156338775ca03c72532da4af0a75d957067e5
alishambo <113910225+alishambo@users.noreply.github.com>
2024-01-03 19:17:42 -0500

    added top songs and artist with album covers

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import datetime
 
 from RecSong import get_users_liked_songs, recommend_songs
+from topsongartist import get_users_top_artists, get_users_top_songs
 
 
 app = Flask(__name__, template_folder='templates')
@@ -41,7 +42,6 @@ def callback():
     if request.args.get('state') != session['state']:
         return 'State token mismatch', 401
 
-   
     code = request.args.get('code')
     token_data = {
         'grant_type': 'authorization_code',
@@ -52,12 +52,14 @@ def callback():
     }
 
     response = requests.post(SPOTIFY_TOKEN_URL, data=token_data)
-    token_json = response.json
+    token_json = response.json()  # Call the json() method here
 
     access_token = token_json['access_token']
     refresh_token = token_json.get('refresh_token')
 
-    # Redirect to the home page
+    # Store the access token in the session
+    session['access_token'] = access_token
+
     return redirect('/home')
 
 @app.route('/home')
@@ -70,6 +72,13 @@ def rec():
     liked_songs = get_users_liked_songs()  # Fetch user's liked songs
     recommended_songs = recommend_songs(liked_songs)  # Get recommendations based on liked songs
     return render_template('rec.html', songs=recommended_songs)  # Note: Changed to rec.html as per your update
+
+@app.route('/top')
+def top():
+    top_songs = get_users_top_songs()
+    top_artists = get_users_top_artists()
+    return render_template('top.html', top_songs=top_songs, top_artists=top_artists)
+
 
 
 
